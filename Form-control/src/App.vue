13fcf4cdbd8a5dd09098eca7f1c593ecdedc9ff0
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-03-25 16:29:58 +0530

    Add files via upload

---
+++ Form-control/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <app-form></app-form>  
+</template>
+
+<script>
+import Form from './components/Form';
+
+export default {
+  components:{
+    'app-form':Form
+  }
+}
+</script>
---
+++ Form-control/src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <label for="">Enter username</label>
+        <input type="text" class="form-control" placeholder="enter username"
+            v-model="textInput">
+        <label for="">Textarea</label>
+       
+       <!-- string interpolation doesn't work with textarea. -->
+        <textarea class="form-control" cols="30" rows="5"
+            v-model="areaText"></textarea>
+        <label for="">CheckBox</label><br>
+        <input type="checkbox" value="Choice1" id="choice1" v-model="checkboxes">Choice1 
+        <input type="checkbox" value="Choice2" id="choice2" v-model="checkboxes">Choice2
+        <input type="checkbox" value="Choice3" id="choice3" v-model="checkboxes">Choice3  
+        <br>
+        <label for="">RADIO</label><br>
+        <input type="radio" value="Male" id="male" v-model="gender" >Male
+        <input type="radio" value="Female" id="female" v-model="gender">Female
+        <div>
+            <p>{{textInput}}</p>
+            <p style="white-space: pre-line">{{areaText}}</p>
+            <p v-for="item in checkboxes" :key="item">{{item}}:::{{gender}}</p>
+            <p>{{checkboxes}}</p>
+            <p>{{gender}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            textInput:'',
+            areaText:'',
+            checkboxes:[],
+            gender:''
+        }
+    }
+}
+</script>
