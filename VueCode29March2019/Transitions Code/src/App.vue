5e84ec8a998cdbd18497664b62fcdda8ba99b2cb
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2019-03-29 15:34:04 +0530

    Add files via upload

---
+++ VueCode29March2019/Transitions Code/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1 class="alert alert-primary">This is an App Component</h1>
+    <button class="btn btn-success" @click="show = !show">Start Transition</button>
+    <hr>
+    <transition name="fade" >
+      <div v-if="show">
+        <h4 class="alert alert-success" >This is some Transition show.</h4>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      show:false
+    }
+  }
+}
+</script>
+
+<style>
+  .fade-enter{
+    opacity: 0;
+  }
+  .fade-enter-active{
+    transition: opacity 2s;
+    /* opacity: 1; */
+  }
+  .fade-enter-to{
+
+  }
+  .fade-leave{
+    opacity: 1
+  }
+  .fade-leave-active{
+    transition: opacity,  2s;
+    opacity: 0;
+  }
+  .fade-leave-to{
+
+  }
+</style>
---
+++ VueCode29March2019/UpdateComputedProperties/src/components/Result.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h5> Counter is --- {{getCounter}}</h5>
+        <h5>{{print}}</h5>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+
+export default {
+    computed:{
+        print(){
+            return 'This is a print computed property';
+        },
+        ... mapGetters([
+        'getCounter'
+    ])
+
+    } 
+    
+    
+   
+   
+    
+}    
+    // computed:{
+    //     count(){
+    //         // return this.$store.state.counter *2;
+    //         return this.$store.getters.getCounter;
+    //     }
+    // }
+    // props:['sendCounter']
+
+</script>
+
+<style>
+
+</style>
